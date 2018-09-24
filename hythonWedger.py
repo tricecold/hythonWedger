1c6598b424ce08a349242c5701c1ed9f46121baa
tricecold <tricecold@gmail.com>
2018-09-23 21:31:09 -0400

    Update hythonWedger.py

--- hythonWedger.py
+++ hythonWedger.py
@@ -14,23 +14,28 @@ start_time = timeit.default_timer()
 hou.hipFile.load("/home/tricecold/pythonTest/HoudiniWedger/HoudiniWedger.hiplc")
 wedger =   hou.parm('/obj/geo1/popnet/source_first_input/seed')
 cache = hou.node('/out/cacheme')
-total_tasks = 4
+total_tasks = 24
 max_number_processes = 8
+fileRange =  abs(hou.evalParmTuple('/out/cacheme/f')[0] - hou.evalParmTuple('/out/cacheme/f')[1])
+target_dir = os.path.dirname(hou.evalParm('/out/cacheme/sopoutput')) + "/"
 
 # Define the function which will be executed within the ThreadPool
-def asyncProcess(wedge=total_tasks):
+def cacheHoudini(wedge=total_tasks):
     wedger.set(wedge)
     cache.render(verbose=False)
+    print('\tWorker maximum memory usage: %.2f (mb)' % (current_mem_usage()))
     
 if __name__ == '__main__':
     
     pool = multiprocessing.Pool(max_number_processes)
     
     for wedge in range(0,total_tasks):
-        pool.apply_async(asyncProcess,args=(wedge,) )
-    
+        pool.apply_async(cacheHoudini,args=(wedge,))
+        
+    #pool.apply_async(files,args=(target_dir,))    
     pool.close() # After all threads started we close the pool
     pool.join() # And wait until all threads are done
+    del pool
 
 print("Parent: this Process ran %s seconds" % str(timeit.default_timer() - start_time))
 
